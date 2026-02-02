bf4efc2f33fb977ed88a92b520fd86794977e2bb
ranjithxploit <cyberexpertranjith@gmail.com>
2026-02-02 15:15:37 +0530

    caesar cipher final

--- caesar_cipher.java
+++ caesar_cipher.java
@@ -10,6 +10,22 @@ public class caesar_cipher {
         System.out.println("Ciphertext: " + ciphertext);
         scanner.close();
     }
+   public static String encrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
 
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                char c = (char) (((ch + shift - 65) % 26) + 65);
+                result.append(c);
+            } else if (Character.isLowerCase(ch)) {
+                char c = (char) (((ch + shift - 97) % 26) + 97);
+                result.append(c);
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
  
 }
