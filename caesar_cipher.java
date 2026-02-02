56ab81a44b4758a0b4a48a7b573ac78fa4d8b905
ranjithxploit <cyberexpertranjith@gmail.com>
2026-02-02 15:28:46 +0530

    playfair cipher

--- caesar_cipher.java
+++ caesar_cipher.java
@@ -27,5 +27,4 @@ public class caesar_cipher {
         }
         return result.toString();
     }
- 
 }
---
+++ playfair_cipher.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class playfair_cipher {
+    static char[][] matrix = new char[5][5];
+    static void generateMatrix(String key) {
+        key=key.toUpperCase().replaceAll("[J]", "I").replaceAll("[^A-Z]", "");
+        String temp ="";
+        for(char c : key.toCharArray()) {
+            if(temp.indexOf(c) ==-1) {
+                temp += c;
+     }
+        }
+        for(char c ='A';c<= 'Z';c++) {
+            if(temp.indexOf(c) == -1 && c != 'J') {
+                temp += c;
+        }
+        }
+        int k = 0;
+        for(int i=0; i<5; i++){
+            for(int j =0;j<5; j++){
+                matrix[i][j] = temp.charAt(k++);
+    }
+    }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter key: ");
+        String key = scanner.nextLine();
+        
+        generateMatrix(key);
+        
+        System.out.println("\nPlayfair Matrix:");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
